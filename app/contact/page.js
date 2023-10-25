58bb6817f41e676f57325ef1008de87119c61eb8
Alessandro Macchitella <alex.mac@albeengineering.com>
2023-10-25 18:15:48 +1100

    version 5.3

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,11 +1,13 @@
-import ContactDetails from './contact';
-
+import Footer from '../components/footer/footer'
+import styles from '../page.module.css'
+import ContactDetails from "./Contact";
+import ContactDetails2 from './contactCol';
 
 
 export default function Contact() {
     return (
     <div>
-      <ContactDetails />
+      <ContactDetails2 />
     </div>
     )
   }
