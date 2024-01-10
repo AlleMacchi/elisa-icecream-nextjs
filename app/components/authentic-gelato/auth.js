d5a8bbdb34847b445ef290fa112520e486b8f5ca
Mario <polmy015@mymail.unisa.edu.au>
2024-01-10 16:13:56 +0800

    Fix Meta

--- app/components/authentic-gelato/auth.js
+++ app/components/authentic-gelato/auth.js
@@ -1,54 +1,74 @@
-import styles from './auth.module.css'
+import styles from "./auth.module.css";
 import Head from "../../components/head/head";
-import GoogleTag from '../GooglTag/GoogleTag';
+import GoogleTag from "../GooglTag/GoogleTag";
 
-export default function AuthenticGelato(){
-    return(
-        <>
-            <Head
-                title='Authentic gelato'
-                content="Elisa Gelato, experience the true taste of Bologna without leaving Sydney"
-                href="https://elisagelato.com.au/" 
-                srcScript="/clarity.js"
-            />
+export default function AuthenticGelato() {
+  return (
+    <>
+      <Head
+        title="Elisa Gelato | Authentic Italian Gelato"
+        content="Indulge in the deliciousness of authentic gelato at Elisa Gelato Randwick. Taste the creamy flavors that will transport you to Italy. Come to experience an a
+            Authentic Italian Gelateria!"
+        href="https://elisagelato.com.au/"
+        srcScript="/clarity.js"
+      />
 
-            <div className={styles.container}>
-                <div className={styles.wrap}>
-                <div className={styles.title}>
-                    <h1>Authentic gelato</h1>
-                </div>
-                
-                <div className={`${styles.text} ${styles.text1}`}>
-                    <p>With Elisa Gelato, experience the true taste of Bologna without leaving Sydney</p>
-                    <br></br>
-                    <p>Our authentic gelato, crafted with traditional recipes and savoir-faire from 
-                        renowned gelato makers in Bologna, offers a unique experience and indulgence at its best. 
-                        With its creamy texture and rich flavours, our gelato is the perfect balance of sweetness and richness. 
-                        Made in small batches using traditional methods and high-quality ingredients, 
-                        our gelato offers the perfect balance of texture and flavour.
-                    </p>
-                    <br></br>
-                    <p>We are not about marketing or international expansion.  
-                        We are all about quality and tradition to offer you the experience of an authentic Italian gelato. 
-                        With every spoon, you'll feel like you've been transported to the streets of Bologna.
-                    </p>
-                </div>
-                <div className={`${styles.img} ${styles.img1}`}>
-                    <img src="Serving.jpg"></img>
-                </div>
-                <div className={`${styles.img} ${styles.img2}`}>
-                <img src="Contact.jpg"></img>
-                </div>
-                <div className={`${styles.text} ${styles.text2}`}>
-                    <p>At Elisa Gelato, we use the traditional way of preparing, storing and serving high quality gelato.  </p>
-                    <br></br>
-                    <p> Gelato is stored in pozzetti – stainless steel cylindrical containers deep inside the counter and covered with lids.  </p>
-                    <br></br>
-                    <p>  Using pozzetti ensures that our gelato remains at the right temperature, so as to maintain its soft, smooth and creamy texture.
-                    </p>
-                </div>
-                </div>
-            </div>
-        </>
-    )
+      <div className={styles.container}>
+        <div className={styles.wrap}>
+          <div className={styles.title}>
+            <h1>Authentic gelato</h1>
+          </div>
+
+          <div className={`${styles.text} ${styles.text1}`}>
+            <p>
+              With Elisa Gelato, experience the true taste of Bologna without
+              leaving Sydney
+            </p>
+            <br></br>
+            <p>
+              Our authentic gelato, crafted with traditional recipes and
+              savoir-faire from renowned gelato makers in Bologna, offers a
+              unique experience and indulgence at its best. With its creamy
+              texture and rich flavours, our gelato is the perfect balance of
+              sweetness and richness. Made in small batches using traditional
+              methods and high-quality ingredients, our gelato offers the
+              perfect balance of texture and flavour.
+            </p>
+            <br></br>
+            <p>
+              We are not about marketing or international expansion. We are all
+              about quality and tradition to offer you the experience of an
+              authentic Italian gelato. With every spoon, you'll feel like
+              you've been transported to the streets of Bologna.
+            </p>
+          </div>
+          <div className={`${styles.img} ${styles.img1}`}>
+            <img src="Serving.jpg"></img>
+          </div>
+          <div className={`${styles.img} ${styles.img2}`}>
+            <img src="Contact.jpg"></img>
+          </div>
+          <div className={`${styles.text} ${styles.text2}`}>
+            <p>
+              At Elisa Gelato, we use the traditional way of preparing, storing
+              and serving high quality gelato.{" "}
+            </p>
+            <br></br>
+            <p>
+              {" "}
+              Gelato is stored in pozzetti – stainless steel cylindrical
+              containers deep inside the counter and covered with lids.{" "}
+            </p>
+            <br></br>
+            <p>
+              {" "}
+              Using pozzetti ensures that our gelato remains at the right
+              temperature, so as to maintain its soft, smooth and creamy
+              texture.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
