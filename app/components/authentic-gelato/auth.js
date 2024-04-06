d3bef1ceccbeb5e8e475b9575491723d4b9a7d6a
Mario <polmy015@mymail.unisa.edu.au>
2024-04-07 07:29:33 +0800

    Test Fix Description

--- app/components/authentic-gelato/auth.js
+++ app/components/authentic-gelato/auth.js
@@ -7,8 +7,8 @@ export default function AuthenticGelato() {
     <>
       <Head
         title="Elisa Gelato | Authentic Italian Gelato | Italian Gelateria Sydney"
-        content="Indulge in the deliciousness of authentic italian gelato at Elisa Gelato in Randwick. Taste the best italian ice cream in Sydney, creamy gelato flavors that will transport you to Italy.If you are looking for a Gelateria Sydney Come to experience an
-            Authentic Italian Gelateria!"
+        content="Tansport your self to Italy wih a taste of the best italian ice cream in Sydney.If you are looking for a Italian Gelateria Sydney Come to experience an
+            Authentic Italian Gelato in Randiwck!"
         href="https://elisagelato.com.au/"
         srcScript="/clarity.js"
       />
--- app/page.js
+++ app/page.js
@@ -1,12 +1,22 @@
-import AuthenticGelato from './components/authentic-gelato/auth'
-import DemoCarousel from './components/carousel/ReactCarousel'
-import styles from './page.module.css'
+import AuthenticGelato from "./components/authentic-gelato/auth";
+import DemoCarousel from "./components/carousel/ReactCarousel";
+import styles from "./page.module.css";
+import Head from "./components/head/head";
 
 export default function Home() {
   return (
     <div>
-      <DemoCarousel />
-      <AuthenticGelato />
+      <>
+        <Head
+          title="Elisa Gelato | Authentic Italian Gelato | Italian Gelateria Sydney"
+          content="Tansport your self to Italy wih a taste of the best italian ice cream in Sydney.If you are looking for a Italian Gelateria Sydney Come to experience an
+            Authentic Italian Gelato in Randiwck!"
+          href="https://elisagelato.com.au/"
+          srcScript="/clarity.js"
+        />
+        <DemoCarousel />
+        <AuthenticGelato />
+      </>
     </div>
-  )
+  );
 }
