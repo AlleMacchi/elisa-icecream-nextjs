99689862cd898cf3786fde8b61132300f77a3288
MarioPolverino-Albe <98942231+MarioPolverino-Albe@users.noreply.github.com>
2024-04-07 07:30:30 +0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,12 +1,19 @@
 import AuthenticGelato from './components/authentic-gelato/auth'
 import DemoCarousel from './components/carousel/ReactCarousel'
 import styles from './page.module.css'
+import Head from 
 
 export default function Home() {
   return (
-    <div>
-      <DemoCarousel />
-      <AuthenticGelato />
-    </div>
+      <>
+        <Head
+          title="Elisa Gelato | Authentic Italian Gelato | Italian Gelateria Sydney"
+          content="Transport yourself to Italy with a taste of the best Italian ice cream in Sydney. If you are looking for an Italian Gelateria Sydney, Come to experience an Authentic Italian Gelato in Randiwck!"
+          href="https://elisagelato.com.au/"
+          srcScript="/clarity.js"
+        />
+        <DemoCarousel />
+        <AuthenticGelato />
+      </>
   )
 }
